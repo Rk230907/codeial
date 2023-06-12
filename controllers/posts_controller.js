16548b5f97535818c8437975542231e54c6ee672
Yash Patel <iyashpatelp@outlook.com>
2023-06-12 23:31:26 +0530

    Update Like

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 const postsMailer = require('../mailers/posts-mailer');
+const Like = require('../models/like');
 
 
 module.exports.create = async function(req, res){
@@ -60,6 +61,10 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
 
         if (post.user == req.user.id){
+
+            // Delete the associated likes for the post and all it's comments likes too
+            await Like.deleteMany({likeable: post, onModel: 'Post'});
+            await Like.deleteMany({_id: {$in: post.comments}});
             
             post.deleteOne();
             
