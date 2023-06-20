ef7d59f0419d067a1346f3e0a2b3ebaef6c737f7
Yash Patel <iyashpatelp@outlook.com>
2023-06-21 01:09:30 +0530

    env changes

--- .env.js
+++ .env.js
@@ -14,32 +14,8 @@ const accessLogStream = rfs('access.log',{
 })
 
 
-const development = {
-    name: 'development',
-    asset_path: '/assets',
-    session_cookie_key: 'blahsomething',
-    db: 'codeial-development',
-    smtp: {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth:{
-            user: 'development.ypatel@gmail.com',
-            pass: 'qbjtiynneablyeko'
-        }
-    },
-    google_client_ID: "187270952782-m7ool49ckt2nqjo3v9aabpta068il6d6.apps.googleusercontent.com",
-    google_client_Secret: "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",
-    google_call_back_URL: "http://localhost:8000/users/auth/google/callback",
-    jwt_secret: 'codeial',
-    morgan:{
-        mode: 'dev',
-        options: {stream: accessLogStream}
-    }
-}
 
-const production = {
+const development = {
     name: 'production',
     asset_path: '/assets',
     session_cookie_key: process.env.CODEIAL_SESSION_COOKIE,
@@ -65,4 +41,4 @@ const production = {
     
 }
 
-module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);   
+module.exports = development;   
